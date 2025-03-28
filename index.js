ca6af225ef46d5a60e09528fb241670189b38be5
AdithyanValloor <165929678+AdithyanValloor@users.noreply.github.com>
2025-03-28 22:32:44 +0530

    Added dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import app from './src/app.js'
 import deleteExpiredAccounts from './src/jobs/deleteExpiredAccounts.js'
 import { connectDb } from './src/config/db.js'
+import dotenv from 'dotenv';
 
+dotenv.config();
 connectDb()
 deleteExpiredAccounts()
 
